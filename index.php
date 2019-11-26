1b54b2b66717a0dd389167e2dc95a9da5c7aa64e
salazhar <canoine.cyril@gmail.com>
2019-11-26 11:22:12 +0100

    quête POO 6 WCS

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+class Speedometer
+{
+    const KM = 1.609;
+    const MILE = 0.621;
+    public static function convertKmToMiles($number)
+    {
+        return $number * Speedometer::MILE;
+    }
+    public static function convertMilesToKm($number)
+    {
+        return $number * Speedometer::KM;
+    }
+}
+echo Speedometer::convertKmToMiles(130);
+echo"<br/>";
+echo Speedometer::convertMilesToKm(88);
